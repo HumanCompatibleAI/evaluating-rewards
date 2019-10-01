ec21b36367deca6835e5af007fd2a12817b17084
Adam Gleave <gleave@google.com>
2019-10-01 09:12:48 -0700

    Reduce flakiness in test_comparisons.
    PiperOrigin-RevId: 272225213

--- tests/test_comparisons.py
+++ tests/test_comparisons.py
@@ -32,18 +32,24 @@ PM_REWARD_TYPES = {
     "ground_truth": {
         "target": "evaluating_rewards/PointMassGroundTruth-v0",
         "loss_ub": 5e-3,
-    },
-    "sparse": {
-        "target": "evaluating_rewards/PointMassSparse-v0",
-        "loss_ub": 4e-2,
+        "rel_loss_lb": 10,
     },
     "dense": {
         "target": "evaluating_rewards/PointMassDense-v0",
         "loss_ub": 4e-2,
+        "rel_loss_lb": 10,
+    },
+    # For sparse and zero, set a low relative error bound, since some
+    # random seeds have small scale and so get a low initial loss.
+    "sparse": {
+        "target": "evaluating_rewards/PointMassSparse-v0",
+        "loss_ub": 4e-2,
+        "rel_loss_lb": 2,
     },
     "zero": {
         "target": "evaluating_rewards/Zero-v0",
-        "loss_ub": 1e-4,
+        "loss_ub": 2e-4,
+        "rel_loss_lb": 2,
     }
 }
 
@@ -54,12 +60,20 @@ class RewardTest(common.TensorFlowTestCase):
   @parameterized.named_parameters(common.combine_dicts_as_kwargs(
       PM_REWARD_TYPES
   ))
-  def test_regress(self, target: str, loss_ub: float):
+  def test_regress(self, target: str, loss_ub: float, rel_loss_lb: float):
+    """Test regression onto target.
+
+    Args:
+      target: The target reward model type. Must be a hardcoded reward:
+          we always load with a path "dummy".
+      loss_ub: The maximum loss of the model at the end of training.
+      rel_loss_lb: The minimum relative improvement to the initial loss.
+    """
     env = gym.make("evaluating_rewards/PointMassLineFixedHorizon-v0")
     venv = vec_env.DummyVecEnv([lambda: env])
 
     dataset_generator = datasets.random_generator(env)
-    dataset = dataset_generator(1e5, 128)
+    dataset = dataset_generator(1e5, 512)
 
     with self.graph.as_default():
       with self.sess.as_default():
@@ -81,12 +95,12 @@ class RewardTest(common.TensorFlowTestCase):
 
     loss = pd.DataFrame(stats["loss"])["singleton"]
     logging.info(f"Loss: {loss.iloc[::10]}")
-    initial_loss = loss.iloc[:5].mean()
+    initial_loss = loss.iloc[0]
     logging.info(f"Initial loss: {initial_loss}")
-    final_loss = loss.iloc[-5:].mean()
+    final_loss = loss.iloc[-10:].mean()
     logging.info(f"Final loss: {final_loss}")
 
-    assert initial_loss / final_loss > 10
+    assert initial_loss / final_loss > rel_loss_lb
     assert final_loss < loss_ub
 
 
