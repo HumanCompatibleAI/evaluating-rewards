d96affb76bef870fe14e5a77815e89916f7758cb
Adam Gleave <adam@gleave.me>
2019-11-24 15:59:25 -0800

    Docker launch script: always bind src

--- runners/launch_docker.sh
+++ runners/launch_docker.sh
@@ -19,13 +19,14 @@ LOCAL_MNT="/mnt/eval_reward"
 CMD="bash"
 FLAGS=""
 if [[ $1 == "jupyter" ]]; then
-        FLAGS="-p 127.0.0.1:8888:8888/tcp -v ${LOCAL_MNT}/src:/evaluating-rewards"
+        FLAGS="-p 127.0.0.1:8888:8888/tcp"
         CMD="pip install jupyter && jupyter notebook --ip 0.0.0.0 --allow-root"
 fi
 
 docker pull ${DOCKER_IMAGE}
 docker run -it --rm \
        -v ${LOCAL_MNT}/data:/root/output \
+       -v ${LOCAL_MNT}/src:/evaluating-rewards \
        -v ${LOCAL_MNT}/mjkey.txt:/root/.mujoco/mjkey.txt \
        ${FLAGS} ${DOCKER_IMAGE} \
        /bin/bash -c "${CMD}"
