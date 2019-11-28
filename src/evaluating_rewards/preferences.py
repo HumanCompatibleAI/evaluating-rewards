Bugfix: off-by-one error in compute_returns for first episode

src/evaluating_rewards/preferences.py
@@ -273,8 +273,8 @@ class PreferenceComparisonTrainer:
         """Returns loss to be optimized given a batch of experience."""
         returns = self._get_returns()
 
-        # self._preference_labels are 0 (first trajectory > second trajectory)
-        # or 1 (second > first), shape (batch_size, ).
+        # self._preference_labels are 1 (first trajectory > second trajectory)
+        # or 0 (second > first), shape (batch_size, ).
         # preference_labels are shape (2, batch_size) and preference_label[i][j]
         # is 1 if trajectory i is preferred in comparison j.
         preference_labels = tf.stack([self._preference_labels, 1 - self._preference_labels])

src/evaluating_rewards/rewards.py
@@ -704,10 +704,10 @@ def compute_returns(
     # To compute returns, we must sum over slices of the flattened reward
     # sequence corresponding to each episode. Find the episode boundaries.
     ep_boundaries = np.where(transitions.dones)[0]
-    # NumPy equivalent of Python ep_boundaries = [0] + ep_boundaries[:-1]
-    idxs = np.pad(ep_boundaries[:-1], (1, 0), "constant")
     # ep_boundaries is inclusive, but reduceat takes exclusive range
-    idxs = idxs + 1
+    ep_boundaries = ep_boundaries + 1
+    # NumPy equivalent of Python idxs = [0] + ep_boundaries[:-1]
+    idxs = np.pad(ep_boundaries[:-1], (1, 0), "constant")
     # Now, sum over the slices.
     ep_returns = {k: np.add.reduceat(v, idxs) for k, v in preds.items()}
 