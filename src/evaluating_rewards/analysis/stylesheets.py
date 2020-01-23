eaedb0e1fb4949850a176ca3ddcd2c612ab0236c
Adam Gleave <adam@gleave.me>
2020-01-23 12:45:45 -0800

    Bugfix: do not delete TEXINPUTS if never existed

--- src/evaluating_rewards/analysis/stylesheets.py
+++ src/evaluating_rewards/analysis/stylesheets.py
@@ -76,7 +76,7 @@ def setup_styles(styles: Iterable[str]) -> Iterator[None]:
         with plt.style.context(styles):
             yield
     finally:
-        if old_tex_inputs is None:
+        if old_tex_inputs is None and "TEXINPUTS" in os.environ:
             del os.environ["TEXINPUTS"]
         else:
             os.environ["TEXINPUTS"] = old_tex_inputs
