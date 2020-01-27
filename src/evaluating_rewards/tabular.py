9aa15a0bfcaea248b6abc861822227f640a660f3
Adam Gleave <adam@gleave.me>
2020-01-26 19:33:22 -0800

    tabular: handle affine transforms, support discounting

--- src/evaluating_rewards/tabular.py
+++ src/evaluating_rewards/tabular.py
@@ -68,43 +68,45 @@ def random_potential(n_states: int, rng: np.random.RandomState = np.random) -> n
     return rng.rand(n_states)
 
 
-def shape(reward: np.ndarray, potential: np.ndarray) -> np.ndarray:
+def shape(reward: np.ndarray, potential: np.ndarray, discount: float) -> np.ndarray:
     """Adds a potential-based shaping to a reward.
 
     Args:
         reward: The three-dimensional reward array.
         potential: The state-only potential function.
+        discount: The discount factor.
 
     Returns:
         reward shaped by potential.
     """
     assert reward.ndim == 3
     assert potential.ndim == 1
-    return reward + potential[np.newaxis, np.newaxis, :] - potential[:, np.newaxis, np.newaxis]
+    new_pot = discount * potential[np.newaxis, np.newaxis, :]
+    old_pot = potential[:, np.newaxis, np.newaxis]
+    return reward + new_pot - old_pot
 
 
-def extract_potential(shaped: np.ndarray, unshaped: np.ndarray) -> np.ndarray:
-    """Reverses `shape`, up to a constant."""
-    assert shaped.ndim == 3
-    assert shaped.shape == unshaped.shape
-    delta = shaped - unshaped
-    potential = delta[0, 0, :]
+def closest_potential(reward: np.ndarray, target: np.ndarray, discount: float) -> np.ndarray:
+    """Finds the squared-error minimizing potential shaping.
 
-    # Make sure potential really is a potential for shaped
-    assert np.allclose(shape(unshaped, potential), shaped)
+    Args:
+        reward: the reward to transform.
+        target: the target to match.
+        discount: the discount factor.
 
-    return potential
-
-
-def closest_potential(reward: np.ndarray, target: np.ndarray) -> np.ndarray:
-    """Finds the potential which makes reward closest to target."""
+    Returns:
+        A state-array of potentials, such that `reward + discount * potential(s') - potential(s)`
+        has minimal least squared-error.
+    """
     assert reward.ndim == 3
     assert reward.shape == target.shape
 
     # Compute coefficients and targets
     n_states, n_actions, _ = reward.shape
     eye = np.eye(n_states)
-    x_vals = eye[np.newaxis, np.newaxis, :, :] - eye[:, np.newaxis, np.newaxis, :]
+    new_pot = discount * eye[np.newaxis, np.newaxis, :, :]
+    old_pot = eye[:, np.newaxis, np.newaxis, :]
+    x_vals = new_pot - old_pot
     x_vals = x_vals.repeat(n_actions, axis=1)
     y_vals = target - reward
 
@@ -113,15 +115,68 @@ def closest_potential(reward: np.ndarray, target: np.ndarray) -> np.ndarray:
     y_vals = y_vals.flatten()
 
     # TODO(): report convergence-related statistics such as singular values?
-    potential, _, _, _ = np.linalg.lstsq(x_vals, y_vals)
+    potential, _, _, _ = np.linalg.lstsq(x_vals, y_vals, rcond=None)
 
     return potential
 
 
-def summary_comparison(reward1: np.ndarray, reward2: np.ndarray) -> Tuple[float, float, np.ndarray]:
+def closest_affine(reward: np.ndarray, target: np.ndarray) -> Tuple[float, float]:
+    """Finds the squared-error minimizing affine transform.
+
+    Args:
+        reward: the reward to transform.
+        target: the target to match.
+
+    Returns:
+        (shift, scale) such that (scale * reward + shift) has minimal squared-error from target.
+    """
+    reward = reward.flatten()
+    target = target.flatten()
+    # Find x such that [1; reward].dot(x) has least-squared error from target
+    # x corresponds to a shift and scaling parameter.
+    a_vals = np.stack([np.ones_like(reward), reward], axis=1)
+    coefs, _, _, _ = np.linalg.lstsq(a_vals, target, rcond=None)
+    assert coefs.shape == (2,)
+
+    return coefs
+
+
+def closest_reward_em(
+    source: np.ndarray, target: np.ndarray, n_iter: int = 100, discount: float = 0.99
+) -> np.ndarray:
+    """Finds the least squared-error reward to target that is equivalent to reward.
+
+    Alternates calls to `closest_potential` and `closest_affine`, in an EM-like approach.
+
+    Args:
+        - source: the source reward.
+        - target: the reward to match.
+        - n_iter: the number of iterations of expectation-maximization.
+        - discount: The discount rate of the MDP.
+
+    Returns:
+        A reward that is equivalent to `source` with minimal squared-error to `target`.
+    """
+    closest_reward = source
+    for _ in range(n_iter):
+        potential = closest_potential(closest_reward, target, discount)
+        closest_reward = shape(closest_reward, potential, discount)
+        shift, scale = closest_affine(closest_reward, target)
+        closest_reward = closest_reward * scale + shift
+    return closest_reward
+
+
+def direct_sq_divergence(source: np.ndarray, target: np.ndarray) -> float:
+    """Direct divergence over uniform transition distribution with squared-error loss."""
+    return np.linalg.norm(source - target) / np.product(source.shape)
+
+
+def summary_comparison(
+    reward1: np.ndarray, reward2: np.ndarray, discount: float
+) -> Tuple[float, float, np.ndarray]:
     """Compares rewards in terms of intrinsic and shaping difference."""
-    potential = closest_potential(reward1, reward2)
-    closest_reward1 = shape(reward1, potential)
+    potential = closest_potential(reward1, reward2, discount)
+    closest_reward1 = shape(reward1, potential, discount)
     intrinisic_difference = np.linalg.norm(reward2 - closest_reward1)
 
     potential_2d = potential[:, np.newaxis]
@@ -131,7 +186,9 @@ def summary_comparison(reward1: np.ndarray, reward2: np.ndarray) -> Tuple[float,
     return intrinisic_difference, shaping_difference, potential
 
 
-def make_shaped_reward(n_states: int, n_actions: int, seed: Optional[int] = None):
+def make_shaped_reward(
+    n_states: int, n_actions: int, discount: float = 1.0, seed: Optional[int] = None
+):
     """Creates random reward, potential and potential-shaped reward."""
     rng = None
     if seed is not None:
@@ -139,7 +196,7 @@ def make_shaped_reward(n_states: int, n_actions: int, seed: Optional[int] = None
 
     reward = random_reward(n_states, n_actions, rng=rng)
     potential = random_potential(n_states, rng=rng)
-    shaped = shape(reward, potential)
+    shaped = shape(reward, potential, discount)
 
     return reward, potential, shaped
 
@@ -155,6 +212,7 @@ def potential_difference(p1, p2):
 def experiment_shaping_comparison(
     n_states: int,
     n_actions: int,
+    discount: float = 1.0,
     reward_noise: Optional[np.ndarray] = None,
     potential_noise: Optional[np.ndarray] = None,
     state_only: bool = True,
@@ -179,10 +237,10 @@ def experiment_shaping_comparison(
     for reward_nm in reward_noise:
         for pot_nm in potential_noise:
             noised_reward = reward + reward_nm * additive_noise
-            noised_shaped = shape(noised_reward, pot_nm * noised_reward_potential)
+            noised_shaped = shape(noised_reward, pot_nm * noised_reward_potential, discount)
 
             # These statistics could be computed in a real application
-            intrinsic, shaping, potential = summary_comparison(noised_shaped, reward)
+            intrinsic, shaping, potential = summary_comparison(noised_shaped, reward, discount)
             intrinsics[(reward_nm, pot_nm)] = intrinsic
             shapings[(reward_nm, pot_nm)] = shaping
 
