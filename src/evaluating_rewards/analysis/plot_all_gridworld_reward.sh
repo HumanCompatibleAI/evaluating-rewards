88afaee8e2ecfea9623827fadb841031337013b1
Adam Gleave <adam@gleave.me>
2020-01-27 20:07:13 -0800

    Add accidentally deleted bash script

---
+++ src/evaluating_rewards/analysis/plot_all_gridworld_reward.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CONFIGS="sparse_goal sparse_goal_shift sparse_goal_scale \
+         dense_goal antidense_goal transformed_goal center_goal \
+         dirt_path cliff_walk sparse_penalty all_zero"
+
+parallel --header : python -m evaluating_rewards.analysis.plot_gridworld_reward \
+          with {config} \
+          ::: config ${CONFIGS}
